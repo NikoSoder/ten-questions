e28b6b6ad406107b0acd02b9ac076d5a79be5f34
Niko <soder.n@gmail.com>
2022-06-18 16:52:22 +0300

    starting to style styles.css

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const question = document.querySelector('h1');
+const questions_div = document.querySelector('.questions');
 
 
 async function getData() {
-    const response = await fetch('https://opentdb.com/api.php?amount=10');
+    const response = await fetch('https://opentdb.com/api.php?amount=10&type=boolean');
     const data = await response.json();
-    question.innerHTML = data.results[0].question;
+    useData(data);
+}
+
+function useData(data) {
+    for(let i = 0; i < data.results.length; i++) {
+        //console.log(data.results[i].question);
+        //console.log(data.results[i].correct_answer);
+        //console.log('-----');
+        const newDiv = document.createElement('div');
+        const question_p = document.createElement('p');
+        const answer_p = document.createElement('p');
+        newDiv.appendChild(question_p);
+        newDiv.appendChild(answer_p);
+        questions_div.appendChild(newDiv);
+        question_p.innerHTML = data.results[i].question;
+        answer_p.innerHTML = data.results[i].correct_answer;
+    }
 }
 
 getData();
@@ -14,7 +30,7 @@ getData();
 
 
 
-
+// practise 
 /*
 fetch('https://opentdb.com/api.php?amount=10')
     .then(function(response) {
