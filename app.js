7bb8c5f914ad0b571f9dff94d0f3fdf92cd5ce23
Niko <soder.n@gmail.com>
2022-06-20 16:10:14 +0300

    show answers button

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const questions_div = document.querySelector('.questions');  // all newDivs goes here
 const loadingAnimation = document.querySelector('.loading');
-
-const allAnswerDivs = document.querySelectorAll('.answer-div');  
-
+const showAnswerButton = document.getElementById('btn');
+let allAnswerButtons = [];
 
 async function getData() {
     const response = await fetch('https://opentdb.com/api.php?amount=10&type=boolean');
@@ -18,32 +17,30 @@ function useData(data) {
         const question_p = document.createElement('p');
         const answerButton = document.createElement('button');
         
-        newDiv.appendChild(question_p);
+        newDiv.appendChild(question_p);   // append elements to elements
         newDiv.appendChild(answerButton);
         questions_div.appendChild(newDiv);
         
         question_p.innerHTML = data.results[i].question;
         answerButton.innerHTML = data.results[i].correct_answer;
         
-        newDiv.classList.add('question');
+        newDiv.classList.add('question');     // add classes to elements
         answerButton.classList.add('answer');
+        answerButton.classList.add('hidden');
         newDiv.classList.add('question');
+        
+        allAnswerButtons.push(answerButton);  // push answers to the list
     }
     
     loadingAnimation.classList.add('hidden');  // hide loading animation when data is displayed
-    /*
-    const allShowAnswerButtons = document.querySelectorAll('.show-answer');
-    const allAnswerDivs = document.querySelectorAll('.answer-div');
-    
-    allShowAnswerButtons.forEach(function(btn) {
-        btn.addEventListener('click', () => {
-            allAnswerDivs.forEach(function(div) {
-                
-                console.log(div);
-            })
-        })
-    });*/
 }
+
+// when user click show answer button remove 'hidden' class from answers 
+showAnswerButton.addEventListener('click', () => {
+    allAnswerButtons.forEach(function(btn) {
+        btn.classList.remove('hidden');
+    })
+});
 
 // main
 const triviaData = getData();
