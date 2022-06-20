68fb57f7a3e75a5870fc4ca3596dfe5c2aac68af
Niko <soder.n@gmail.com>
2022-06-20 14:39:50 +0300

    changing styles

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const questions_div = document.querySelector('.questions');  // all newDivs goes here
-const loadingAnimation = document.querySelector('.loading');  
+const loadingAnimation = document.querySelector('.loading');
+
+const allAnswerDivs = document.querySelectorAll('.answer-div');  
 
 
 async function getData() {
@@ -15,16 +17,32 @@ function useData(data) {
         const newDiv = document.createElement('div');  // question_p and answerButton goes here
         const question_p = document.createElement('p');
         const answerButton = document.createElement('button');
+        
         newDiv.appendChild(question_p);
         newDiv.appendChild(answerButton);
         questions_div.appendChild(newDiv);
+        
         question_p.innerHTML = data.results[i].question;
         answerButton.innerHTML = data.results[i].correct_answer;
+        
         newDiv.classList.add('question');
         answerButton.classList.add('answer');
         newDiv.classList.add('question');
     }
+    
     loadingAnimation.classList.add('hidden');  // hide loading animation when data is displayed
+    /*
+    const allShowAnswerButtons = document.querySelectorAll('.show-answer');
+    const allAnswerDivs = document.querySelectorAll('.answer-div');
+    
+    allShowAnswerButtons.forEach(function(btn) {
+        btn.addEventListener('click', () => {
+            allAnswerDivs.forEach(function(div) {
+                
+                console.log(div);
+            })
+        })
+    });*/
 }
 
 // main
